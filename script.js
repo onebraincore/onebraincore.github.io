fa93ba4c45e06f80337f9e6993e4ca8bcb27bcb0
onebraincore <129500112+onebraincore@users.noreply.github.com>
2023-04-06 19:09:18 +0200

    update

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 /* Timer*/
 
+if (window.location.href.indexOf('index2.html') > -1) {
+
+
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 5, 2023 15:37:25").getTime();
+var countDownDate = new Date("May 5, 2023 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -28,6 +31,9 @@ var x = setInterval(function() {
     document.getElementById("brain-timer").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+  // Your JavaScript code here
+}
 
 /* END Timer */
 
