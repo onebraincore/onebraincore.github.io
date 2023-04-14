b8997f24599c8f813e0c6e5d89c3f336cd1d26ff
onebraincore <129500112+onebraincore@users.noreply.github.com>
2023-04-14 21:05:31 +0200

    particles added

--- script.js
+++ script.js
@@ -348,3 +348,17 @@ if (window.location.href.indexOf('index.html') > -1) {
 }
 
 /* END Anchor navigation hiding */
+
+/* Particles run */
+
+tsParticles
+  .loadJSON("tsparticles", "particles.json")
+  .then((container) => {
+    console.log("callback - tsparticles config loaded");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+   
+   
+/* END particles run */
