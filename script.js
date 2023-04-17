b8ccf141dbc821edb37b034cd03c738114578d9f
onebraincore <129500112+onebraincore@users.noreply.github.com>
2023-04-17 09:09:26 +0200

    script.js timer block unlocked

--- script.js
+++ script.js
@@ -20,10 +20,7 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-days = 30;
-hours = 0;
-minutes = 0;
-seconds = 0;
+
   // Display the result in the element with id="demo"
   document.getElementById("brain-timer").innerHTML = "<div class = 'timer-days d-inline'>" + days + "</div>"+":"+"<div class = 'timer-hours d-inline'>" + hours+ "</div>"+":" +"<div class = 'timer-minutes d-inline'>"+ minutes +"</div>"+":" +"<div class = 'timer-seconds d-inline'>" + seconds + "</div>";
 
