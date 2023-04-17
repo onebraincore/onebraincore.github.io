2a4f6876c4109aa7524d9c5dc4aa2982823422a4
onebraincore <129500112+onebraincore@users.noreply.github.com>
2023-04-17 09:05:09 +0200

    timer is on

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ if (window.location.pathname== '/') {
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("May 5, 2023 15:37:25").getTime();
+var countDownDate = new Date("May 17, 2023 09:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -216,6 +216,7 @@ var tag = document.createElement('script');
       }
       function onPlayerReady(event) {
         // event.target.playVideo();
+        event.target.style.filter = 'invert(0)';
       }
 
       var done = false;
