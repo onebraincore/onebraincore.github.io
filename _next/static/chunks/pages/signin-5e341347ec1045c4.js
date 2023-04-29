8dd3b378cc77a8550edc74e26f36b4e301573022
onebraincore <129500112+onebraincore@users.noreply.github.com>
2023-04-29 16:45:12 +0200

    css update

---
+++ _next/static/chunks/pages/signin-5e341347ec1045c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[176],{4758:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/signin",function(){return _(9021)}])},9021:function(n,u,_){"use strict";_.r(u);var t=_(5893);_(7294);var i=_(6283);let e=()=>(0,t.jsx)(i.Z,{});u.default=e}},function(n){n.O(0,[67,283,774,888,179],function(){return n(n.s=4758)}),_N_E=n.O()}]);
